72d82d0af184dd09dddc34d3dfdb652bfaa884ff
Viewerport <canadian.turkey.lore@gmail.com>
2025-08-10 12:50:05 -0500

    Update convertToParkerText.js

--- convertToParkerText.js
+++ convertToParkerText.js
@@ -1,5 +1,5 @@
 function convertToParkerText(){
-	var originalText = document.getElementById("normalText");
+	var originalText = document.getElementById("normalText").value;
 	var newText = "";
 
 	var keyboard = [
@@ -7,6 +7,23 @@ function convertToParkerText(){
 		["A", "S", "D", "F", "G", "H", "J", "K", "L"],
 		["Z", "X", "C", "V", "B", "N", "M"]
 	];
+
+	var constrain = function(val, small, big){
+		if (val < small){
+			return small;
+		}
+		if (val > big){
+			return big;
+		}
+		return val;
+	};
+
+	var isCapital = function(val){
+		if (val === val.toUpperCase()){
+			return true;
+		}
+		return false;
+	};
 
 	for (var i = 0; i < originalText.length; i ++){
 		var letter = originalText.charAt(i);
@@ -16,7 +33,7 @@ function convertToParkerText(){
 		var rand2 = Math.round(Math.sin((i + 100) * originalText.length * 1000) / 2 + 0.5) === 0 ? -1 : 1;
 
 		for (var j = 0; j < keyboard.length; j ++){
-			for (var k = 0; k < keyboard[j].length; j ++){
+			for (var k = 0; k < keyboard[j].length; k ++){
 				if (keyboard[j][k] === letter.toUpperCase()){
 					keyboardIndex = [j, k];
 				}
@@ -26,10 +43,15 @@ function convertToParkerText(){
 		keyboardIndex[0] = constrain(keyboardIndex[0] + rand1, 0, 2);
 		keyboardIndex[1] = constrain(keyboardIndex[1] + rand2, 0, keyboard[keyboardIndex[0]].length - 1);
 
-		newText += keyboard[keyboardIndex[0]][keyboardIndex[1]];
-		console.log(keyboard[keyboardIndex[0]][keyboardIndex[1]]);
+		if (!isCapital(keyboard[keyboardIndex[0]][keyboardIndex[1]])){
+			newText += keyboard[keyboardIndex[0]][keyboardIndex[1]].toLowerCase();
+		}else
+		if (isCapital(keyboard[keyboardIndex[0]][keyboardIndex[1]])){
+			newText += keyboard[keyboardIndex[0]][keyboardIndex[1]];
+		}else{
+			newText += letter;
+		}
 	}
-	console.log(String(newText));
 	
 	document.getElementById("parkerText").innerHTML = newText;
 }
